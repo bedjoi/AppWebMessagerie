b75d3ff090f4fa9156836f6400620761f87f7ccd
obedi bedjo <obedindevu@gmail.com>
2022-11-08 15:35:26 +0200

    feat: creating og register function

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -8,28 +9,42 @@ function App() {
   const [loginUsername, setLoginUsername] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
 
+  const register= ()=>{
+    axios({
+      method: "POST",
+      date: {
+        username: registerUsername,
+        password: registerPassword
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/register",
+    })
+  };
+  const login= ()=>{};
+  const getUser= ()=>{};
+
   return (
     <div className="App">
       
       <div>
           <h1>Register</h1>
-          <input placeholder="UserName" />
-          <input placeholder="Password" />
-          <button>submit</button>
+          <input placeholder="UserName"  onChange ={ e => setRegisterUsername(e.target.value)} />
+          <input placeholder="Password" onChange ={ e => setRegisterPassword(e.target.value)} />
+          <button onClick={register}>submit</button>
 
       </div>
       
       <div>
           <h1>login</h1>
-          <input placeholder="UserName" />
-          <input placeholder="Password" />
-          <button>submit</button>
+          <input placeholder="UserName" onChange = { e=> setLoginUsername(e.target.value)} />
+          <input placeholder="Password" onChange={ e => setLoginPassword(e.target.value)} />
+          <button onClick={login}>submit</button>
 
       </div>
       
       <div>
           <h1>get User</h1>
-          <button>submit</button>
+          <button onClick={getUser}>submit</button>
 
       </div>
 
